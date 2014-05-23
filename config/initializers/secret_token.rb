39f562c59e7d599bc5a64cf72dafe11fa96be504
aabrahamians <aabrahamians@gmail.com>
2014-05-23 16:22:29 -0700

    first step

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SentiMapper::Application.config.secret_key_base = '982dcf4f26625092c6f123a17d0dc4a35586b9a53fbca8c6de8d5ce755fb71b181b588b945d7cee9c4a98dc8d35916f2fb741db0c7435e2d6e662c109db344e2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SentiMapper::Application.config.session_store :cookie_store, key: '_SentiMapper_session'
