9e7cef88273a80dac8715ec28af32790529762e4
aabrahamians <aabrahamians@gmail.com>
2014-05-31 12:52:37 -0700

    different color markers

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -5,6 +5,10 @@ class TweetsController < ApplicationController
   	json = File.read(Rails.root + "app/assets/tweets.json")
   	@tweets = JSON.parse(json)
   	puts Rails.root 
+  	@green = ("asset/images/green-dot.png")
   end
+
+
+
 
 end
