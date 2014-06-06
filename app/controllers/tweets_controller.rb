8fc8618b59029067198fac52afe51ed52f982efc
Albert Liao <al_liao@yahoo.com>
2014-06-05 19:22:03 -0700

    adding json view

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,10 +1,22 @@
 require 'json'
+require 'open-uri'
 
 class TweetsController < ApplicationController
   def index
   	# json = File.read(Rails.root + "app/assets/tweets.json")
   	# @tweets = JSON.parse(json)
-  	@tweets = Tweet.all.where("CAST(sentiment as float) != 0.0")
+  	# @tweets = Tweet.all.where("CAST(sentiment as float) != 0.0")
+
+	response = open('http://sentimapper.heroku.com/tweets/index.json')
+	@tweets = JSON.parse(response)
+  end
+
+  def show
+    @tweets = Tweet.all.where("CAST(sentiment as float) != 0.0")
+	respond_to do |format|
+    	format.html { }
+    	format.json { render json: @tweets }
+    end
   end
 
   def reload
