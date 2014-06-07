cb51de3128fe99d927d0eb64eee3000fefa6be49
jeffreytakaki <jeffreytakaki@Jeffreys-MacBook-Pro.local>
2014-06-06 18:57:02 -0700

    with incomplete rspec controller testing

--- spec/controllers/tweets_controller_spec.rb
+++ spec/controllers/tweets_controller_spec.rb
@@ -1,27 +1,68 @@
 require 'spec_helper'
 
 describe TweetsController do
-	# context 'when format is HTML' do
-	# 	describe "GET 'index' " do
-	# 		it "renders index template for index" do
-	# 			get :index
-	# 			expect(response).to render_template :index
-	# 		end
-	# 		it "returns http success" do
-	# 			Tweet.should_receive(:all).and_return "All our posts"
-	# 			get 'index'
-	# 			expect(assigns(:posts)).to eq "All our posts"
-	# 			response.should be_success
-	# 		end
-	# 	end
+	
+		let :valid_attributes do
+	   		{
+		      :tweet_id => "472858894062985216",
+		      :user_name => "Amber Mesker",
+		      :text => "Super duper secret creative project for adboom with the talented leviholiman. #hastagads @ Venice Beach http://t.co/XVNt8iHGKc",
+		      :latitude => "33.98589209",
+		      :longitude => "-118.4727913",
+		      :sentiment => "-4",
+		      :created_at => "2014-06-07T00:59:35.000Z",
+		      :updated_at => "2014-06-07T00:59:35.444Z"
+	    	}
+  		end
 
-		# describe "GET 'show' " do
-		# 	it 'returns http success' do
-		# 		Tweet.should_receive(:find).with("123").and_return("A post")
-		# 		get 'show', {id: 123}
-		# 		expect(assigns(:tweet)).to eq "A post"
-		# 		response.should be_success
-		# 	end
-		# end
-	# end
+	describe 'GET index' do
+	
+		before do
+			get :index
+		end	
+
+		let! :tweet1 do
+    		Tweet.create! valid_attributes
+    	end
+
+		it 'should render index template' do
+			expect(response).to render_template :index
+		end
+
+		it "should succeed" do
+    		expect(response).to be_success
+    	end
+
+		it "should assign tweets to all tweets" do
+			expect(assigns(:tweets)).to include(tweet1)
+	    end
+
+	end	
+
+	describe "html format" do
+      it "should render the index template" do
+        get :index
+        expect(response).to render_template :index
+      end
+    end
+
+    describe "json format" do
+      xit "should render the tweets in json" do
+        get :index, :format => :json
+        expect(JSON.parse(response.body)).to have(8).objects
+      end
+    end	
+
+
+
 end
+
+
+# test @tweets
+# 	render json, templates
+# 	assigns
+# 	test where clauses
+# 	relish
+# 	everydayrails guy
+# 	render HTML
+# 	render json
