0c3d1a49f0d356b4ed2394c2e2b8134d8a039e33
aabrahamians <aabrahamians@gmail.com>
2014-06-08 21:48:12 -0700

    faked data

--- app/assets/javascripts/histogram.js
+++ app/assets/javascripts/histogram.js
@@ -3,27 +3,19 @@ $( document ).ready(function() {
   var w = 600;
   var h = 250;
 
-  var dataset = [ 
-  	{ key: 0, value: 5 },
-  	{ key: 1, value: 10 },
-  	{ key: 2, value: 13 },
-  	{ key: 3, value: 19 },
-  	{ key: 4, value: 21 },
-  	{ key: 5, value: 25 },
-  	{ key: 6, value: 22 },
-  	{ key: 7, value: 18 },
-  	{ key: 8, value: 15 },
-  	{ key: 9, value: 13 },
-  	{ key: 10, value: 11 },
-  	{ key: 11, value: 12 },
-  	{ key: 12, value: 15 },
-  	{ key: 13, value: 20 },
-  	{ key: 14, value: 18 },
-  	{ key: 15, value: 17 },
-  	{ key: 16, value: 16 },
-  	{ key: 17, value: 18 },
-  	{ key: 18, value: 23 },
-  	{ key: 19, value: 25 } ];
+   var dataset = [ 
+    { key: -5, value: 5 },
+    { key: -4, value: 10 },
+    { key: -3, value: 13 },
+    { key: -2,value: 19 },
+    { key: -1, value: 21 },
+    { key: 0, value: 25 },
+    { key: 1, value: 22 },
+    { key: 2, value: 18 },
+    { key: 3, value: 15 },
+    { key: 4, value: 13 },
+    { key: 5, value: 11 }];
+
 
   var xScale = d3.scale.ordinal()
   				.domain(d3.range(dataset.length))
